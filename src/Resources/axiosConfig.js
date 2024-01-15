1ee17e8329023665bf8038b2801de46d5e062634
lalisa-1 <lelisawak@gmail.com>
2024-01-15 10:35:52 -0600

    baseurl commit

--- src/Resources/axiosConfig.js
+++ src/Resources/axiosConfig.js
@@ -1,4 +1,12 @@
 import axios from "axios";
 
-const axiosBase = axios.create(`${process.env.baseURL}`);
+// const axiosBase = axios.create({ baseURL: "http://localhost:5500/api" });
+// export default axiosBase;
+
+// export default axiosBase;
+
+const axiosBase = axios.create({
+	baseURL: import.meta.env.VITE_BACKEND_BASE_URL,
+});
+
 export default axiosBase;
